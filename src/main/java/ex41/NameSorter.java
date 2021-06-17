63147703e2b442076625b924f450787b0cb9a106
kiwibrie <brianne0128@gmail.com>
2021-06-16 22:40:51 -0500

    Initial commit

---
+++ src/main/java/ex41/NameSorter.java
@@ -0,0 +1,18 @@
+package ex41;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 3 Solution
+ *  Copyright 2021 Brianne Juntunen
+ */
+
+public class NameSorter {
+
+    public static void main(String[] args){
+        //read the file, line by line, inserting into an array
+        //close input file
+        //sort array, return sorted array
+        //print array to output file
+        //close file
+
+    }
+}
